27fc475a427f597d396e244071e8be34b2998139
thewaqasdev <codewithwaqaspro@gmail.com>
2025-11-12 03:30:55 +0500

    Create src/extension.ts file for Continue VS Code extension activation and deactivation

---
+++ extensions/vscode-extension/src/extension.ts
@@ -0,0 +1,15 @@
+// src/extension.ts
+
+import * as vscode from 'vscode';
+
+// This method is called when your extension is activated
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "Continue" is now active!');
+
+    // Add your activation logic here.
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {
+    console.log('Your extension "Continue" is now deactivated!');
+}
